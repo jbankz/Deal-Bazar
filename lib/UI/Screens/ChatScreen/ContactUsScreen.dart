9544f5efb18f47e6ae1ebeabd6732b78fe42f074
JayCode <vsquare396@gmail.com>
2022-05-19 09:44:16 +0100

    Updated UI

--- lib/UI/Screens/ChatScreen/ContactUsScreen.dart
+++ lib/UI/Screens/ChatScreen/ContactUsScreen.dart
@@ -1,28 +1,37 @@
 import 'package:deal_bazaar/Core/Constants/Animations.dart';
-import 'package:deal_bazaar/Core/Constants/Assets.dart';
 import 'package:deal_bazaar/Core/Constants/Colors.dart';
-import 'package:deal_bazaar/UI/Screens/CartScreen/MyCartScreen.dart';
 import 'package:deal_bazaar/UI/shared/textview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
-
-import '../../shared/appbar.dart';
-import '../BrandPage/BrandPage.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+import '../../shared/appbar.dart';
+
 class Data {
-  final String? image;
+  final Widget? image;
+  final IconData? icon;
   final String? title;
   final String? link;
 
-  Data({this.image, this.title, this.link});
+  Data({this.image, this.icon, this.title, this.link});
 
   static List<Data> medias() {
     List<Data> _medias = [];
-    Data _d = Data(image: '', title: '', link: '');
+    Data _d = Data(
+        icon: Icons.whatsapp,
+        title: 'WhatsApp',
+        link: 'https://wa.me/+923091433933?text=Hey!');
+    _medias.add(_d);
+    _d = Data(
+        icon: Icons.sms, title: 'Phone', link: 'sms:+923091433933?body=Hey!');
+    _medias.add(_d);
+    _d = Data(
+        image: Lottie.asset(WhatsAppAnimtion, height: 50),
+        title: 'Text us',
+        link: 'https://m.me/33mshafiq');
     _medias.add(_d);
     return _medias;
   }
@@ -53,7 +62,7 @@ class ChatScreen extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             SizedBox(height: 20.h),
-            Container(
+            /*   Container(
               width: _size.width.w,
               padding: EdgeInsets.all(8.w),
               decoration: BoxDecoration(
@@ -175,6 +184,47 @@ class ChatScreen extends StatelessWidget {
                 ),
               ),
             ),
+           */
+
+            ...Data.medias()
+                .map((m) => GestureDetector(
+                      onTap: () async => await launch(m.link!),
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 8.w, vertical: 4.h),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          border: Border.all(
+                            color: yellowColor,
+                            width: 2,
+                          ),
+                        ),
+                        child: Row(
+                          children: [
+                            m.image != null
+                                ? Lottie.asset(WhatsAppAnimtion, height: 50)
+                                : Icon(m.icon,
+                                    size: 30.sp, color: Colors.green),
+                            SizedBox(width: 10.w),
+                            Expanded(
+                              child: Text(
+                                m.title ?? '',
+                                style: GoogleFonts.roboto(
+                                  fontSize: 14.sp,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                            Spacer(),
+                            Icon(
+                              Icons.arrow_forward,
+                              size: 20,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ))
+                .toList()
           ],
         ),
       ),
